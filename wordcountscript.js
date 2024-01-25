7afeb7ff99c7f607fd41f6d258f91c0b0859bdeb
David Dallas <Daviddallas606@gmail.com>
2024-01-25 07:27:03 +0000

    line counter added in

--- wordcountscript.js
+++ wordcountscript.js
@@ -3,12 +3,30 @@
 const fs = require('fs');
 const readline = require('readline');
 
+
 const countBytes = (filePath) => {
     try {
-        const content = fs.readFileSync(filePath, 'utf-8');
+        const content = fs.readFileSync(filePath);
         return content.length;
     } catch (error) {
         console.error(`Error reading file: ${error.message}`);
+        return 0;
+    }
+};
+
+const countLines = (filePath) => {
+    try {
+        const content = fs.readFileSync(filePath, 'utf-8');
+        const splitText = content.split(/\r?\n/);
+        const lastLineIsEmpty = splitText[splitText.length - 1] === '';
+        if(lastLineIsEmpty){
+            return splitText.length -1;
+        } else{
+            return splitText.length;
+        }
+
+    } catch (error) {
+        console.error(`Error reading file: ${error}`);
         return 0;
     }
 };
@@ -17,5 +35,7 @@ const cliContent = process.argv.slice(2);
 
 
 if(cliContent[0] === '-c'){
-    console.log(countBytes(cliContent[1]))
+    console.log(countBytes(cliContent[1]));
+} else if (cliContent[0] === '-l'){
+    console.log(countLines(cliContent[1]));
 }
